a92d6eaa9352b028f609844093f4b6362a458cb6
Logan Powell <loganp@tepper.cmu.edu>
2020-04-27 09:38:43 -0400

    prevent REPL play from triggering jest tests during watch mode

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,14 @@ module.exports = {
   transform: {
     // registers babel.config.js with jest
     "^.+\\.js$": "babel-jest",
+    // alternative to preset: "ts-jest"
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
   // explicitly `(` + include + `)` any libs using ESM modules
   transformIgnorePatterns: ["node_modules/?!(@-0)"],
+  // keep playing in the REPL (Quokka) from rerunning tests
+  // in watch mode
+  watchPathIgnorePatterns: ["^.+\\.repl.(ts|js)$"],
 }
 
 // TROUBLE SHOOTING: https://github.com/facebook/jest/issues/2081#issuecomment-619441551
