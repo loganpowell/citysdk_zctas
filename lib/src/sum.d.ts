5af92f478f00a222cecb47ccc64895e6fb980b59
Logan Powell <loganp@tepper.cmu.edu>
2020-06-15 11:18:29 -0400

    refreshing

--- lib/src/sum.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const sum: (a: any, b: any) => any;
-export declare const product: (a: any, b: any) => number;
-export declare const bloop = "bloop";
-export declare const bleep = "bleep";
-export declare const blop = "blop";
