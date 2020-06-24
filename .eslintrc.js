781ca9ce793531a6de214a6935db608f50bf3811
Logan Powell <loganp@tepper.cmu.edu>
2020-06-24 17:29:19 -0400

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     plugins: ["@typescript-eslint", "prettier"],
     extends: [
-        "plugin:prettier/recommended",
+        //"plugin:prettier/recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
     ],
