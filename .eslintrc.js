bdb8f444d4f73a48bbdfa9d9d09ef2837f17ea08
Logan Powell <loganp@tepper.cmu.edu>
2020-06-29 15:17:23 -0400

    regeneratorruntime bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2018,git
         sourceType: "module",
     },
     plugins: ["@typescript-eslint", "prettier"],
